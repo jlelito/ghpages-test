9abe5e0e1fee4e985f62771b9152e7c7fe3e56f8
jlelito <joshualelito@gmail.com>
2020-01-07 15:02:31 -0500

    Updates

---
+++ precache-manifest.1c3a54662ff92a47d9c6c3d1942bf3d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e793bad4aab9e22dd2d84d9d135049ee",
+    "url": "/ghpages-test/index.html"
+  },
+  {
+    "revision": "7bbd7d0b3d94bf3b76a8",
+    "url": "/ghpages-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04f835155e69505e81c9",
+    "url": "/ghpages-test/static/js/2.8abce67c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ghpages-test/static/js/2.8abce67c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7bbd7d0b3d94bf3b76a8",
+    "url": "/ghpages-test/static/js/main.9436aa90.chunk.js"
+  },
+  {
+    "revision": "7721d3b82ce034448d7f",
+    "url": "/ghpages-test/static/js/runtime-main.a1573bd2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ghpages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
