2c08563ed40c3a78a272a7dc3bb1a993788f2f6e
jlelito <joshualelito@gmail.com>
2020-01-07 15:17:14 -0500

    testing re-deploying gh-pages

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          JOSH LELITO'S GITHUB PAGES REACT APP
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is testing to learn how to upload to Github Pages
         </a>
       </header>
     </div>
